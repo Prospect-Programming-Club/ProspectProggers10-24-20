40edb9c9f164ede7b9514f1fac04218fb163e0a8
danicarm <73402461+danicarm@users.noreply.github.com>
2020-10-24 14:27:46 -0700

    Code for reading JSON

--- Server/textfile.js
+++ Server/textfile.js
@@ -1,12 +1,14 @@
-<h1>Question: Do dogs bark?</h1>
-var myObj = {name: "Dan", Response: 'yes'};
-var myJSON = JSON.stringify(myObj);
-window.location = "demo_json.php?x=" + myJSON;
+Const = fs = require ('fs');
 
-var myObj = {name: 'lan', Response: 'maybe'}
-var myJSON = JSON.stringify(myObj);
-window.location = window.location = "demo_json.php?x=" + myJSON;
-
-var myObj = {name: 'Span', Response: 'No'}
-var myJSON = JSON.stringify(myObj);
-window.location = window.location = "demo_json.php?x=" + myJSON;
+fs.readFile('./example.jason', 'utf-8',(err, jsonString) =>{
+    if (err){
+        Console.log(err);
+    }else{
+    try {
+        constdata = JSON.parse(jsonString);
+        console.log(jsonString);
+    }   catch (err) {
+        console.log('Error parsing JSON', err)
+    }
+}
+});
