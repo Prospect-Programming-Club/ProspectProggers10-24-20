16e734bbc72f27246bea8dbf188f36b8c0d7c3c7
danicarm <73402461+danicarm@users.noreply.github.com>
2020-10-24 13:29:39 -0700

    Json
    This is what I have been doing.

---
+++ Server/textfile.js
@@ -0,0 +1,12 @@
+<h1>Question: Do dogs bark?</h1>
+var myObj = {name: "Dan", Response: 'yes'};
+var myJSON = JSON.stringify(myObj);
+window.location = "demo_json.php?x=" + myJSON;
+
+var myObj = {name: 'lan', Response: 'maybe'}
+var myJSON = JSON.stringify(myObj);
+window.location = window.location = "demo_json.php?x=" + myJSON;
+
+var myObj = {name: 'Span', Response: 'No'}
+var myJSON = JSON.stringify(myObj);
+window.location = window.location = "demo_json.php?x=" + myJSON;
